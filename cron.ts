5f2326278c6c7e12637921fb06dcb643f17d2a74
Bahoz <julienbraultcherif@gmail.com>
2025-11-13 20:19:00 +0100

    add right cron hour

--- cron.ts
+++ cron.ts
@@ -5,7 +5,7 @@ const baseUrl = process.env.API_URL;
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 cron.schedule(
-  "58 19 * * *",
+  "30 12 * * *",
   async () => {
     console.log(
       "Création de la vidéo lancé à ",
