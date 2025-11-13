995aa913226c8916b8c9a638ba075dc8875bd977
Bahoz <julienbraultcherif@gmail.com>
2025-11-13 19:54:08 +0100

    test cron with new systems

--- cron.ts
+++ cron.ts
@@ -5,7 +5,7 @@ const baseUrl = process.env.API_URL;
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 cron.schedule(
-  "30 12 * * *",
+  "58 19 * * *",
   async () => {
     console.log(
       "Création de la vidéo lancé à ",
