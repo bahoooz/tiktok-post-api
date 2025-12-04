7dc01c4113cacd2e94739484f247d11fe78708e1
Bahoz <julienbraultcherif@gmail.com>
2025-12-04 19:56:51 +0100

    test cors

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://192.168.1.185:5173",
   "http://192.168.1.175:5173",
-  "https://10banc.com"
+  "https://10banc.com",
+  "https://www.10banc.com"
 ];
 
 const corsOptions: CorsOptions = {
